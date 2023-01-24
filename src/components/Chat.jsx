57810987f8a549b435b3b9fcb655c6e75d12ba57
Suman Paul <sumanpaul91225@gmail.com>
2023-01-24 17:43:38 +0530

    Added more menu

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -7,17 +7,27 @@ import { ChatContext } from '../context/chatContext';
 const Chat = () => {
     const {data} = useContext(ChatContext);
 
+    var menu = document.querySelector(".pop-up");
+    const handlePop = async() => {
+        menu.classList.toggle("func-pop");
+    }
+
     return (
         <div className='chat'>
             <div className="chat-info">
                 <span>{data.user?.displayName}</span>
-                {/* <div className="profile"> */}
-                    {/* <img src={data.user?.photoURL} alt="" /> */}
-                {/* </div> */}
+                {/* <div className="profile">
+                    <img src={data.user?.photoURL} alt="" />
+                </div> */}
+                <div className="pop-up">
+                    <div className="pop-item">New Group</div>
+                    <div className="pop-item">New Broadcast</div>
+                    <div className="pop-item">Linked Devices</div>
+                    <div className="pop-item">Starred Devices</div>
+                    <div className="pop-item">Settings</div>
+                </div>
                 <div className="chat-icons">
-                    <i class="fa-solid fa-video"></i>
-                    <i class="fa-solid fa-user-plus"></i>
-                    <i class="fa-solid fa-ellipsis"></i>
+                    <i class="fa-solid fa-ellipsis" onClick={() =>handlePop()}></i>
                 </div>
             </div>
             <Messages />
